91f233410ca1b002e5427fad05f9e959be60a009
Jay Tolentino <jltolent@gmail.com>
2022-04-19 11:29:56 -0700

    realistic solution added plus cleaned up template

--- simple_cap_realistic.rb
+++ simple_cap_realistic.rb
@@ -4,24 +4,44 @@
 # Learn more at https://news.crunchbase.com/news/cap-tables-share-structures-valuations-oh-case-study-early-stage-funding/
 
 class Shareholder
-  # Feel free to adjust this class as needed
-
   attr_accessor :name, :num_shares, :percent, :price, :value
 
-  def initialize
+  def initialize(name, num_shares, percent, price)
+    @name = name
+    @num_shares = num_shares
+    @percent = percent
+    @price = price
+
+    recalculate_value
+  end
+
+  def recalculate_value
+    @value = @num_shares * @price
+  end
+
+  def to_s
+    "#{@name} => num_shares: #{@num_shares}, percent: #{@percent}, price: #{@price}, value: #{@value}"
   end
 end
 
 class SAFE
-  # Feel free to adjust this class as needed
+  attr_accessor :name, :paid_amount, :discount, :cap
 
-  attr_accessor :name, :paid_amount, :discount, :val_cap
+  def initialize(name, paid_amount, discount, cap)
+    @name = name
+    @paid_amount = paid_amount
+    @discount = discount
+    @cap = cap
+  end
 
-  def initialize
+  def to_s
+    "[SAFE] #{@name} => paid_amount: #{@paid_amount}, discount: #{@discount}, cap: #{@cap}"
   end
 end
 
 class SimpleCap
+  attr_accessor :shareholders, :safes
+
   # Part 1: Create the initial cap table
   # When incorporating a company, founders will create an initial cap table
   # including equity split amongst the founders. Any unused shares should be
@@ -42,9 +62,26 @@ class SimpleCap
   # price_per_share (optional): float representing USD value of each share
   # e.g. 0.001 for $0.001
   def initialize(founders_to_equity_percent, total_shares, price_per_share)
+    @shareholders = []
+    @safes = []
 
-    # your code here...
+    founders_to_equity_percent.map do |name, equity_percent|
+      @shareholders.push(Shareholder.new(
+        name,
+        (total_shares * equity_percent).round,
+        equity_percent,
+        price_per_share,
+      ))
+    end
 
+    # Add options pool
+    options_equity_percent = (1 - @shareholders.sum(&:percent)).round(4)
+    @shareholders.push(Shareholder.new(
+      'Options pool',
+      (total_shares * options_equity_percent).round,
+      options_equity_percent,
+      price_per_share,
+    ))
   end
 
   # Part 2: Add a SAFE round
@@ -79,9 +116,7 @@ class SimpleCap
   # Output:
   # No output required, but up to your discretion
   def add_safes(investors_to_terms)
-
-    # your code here...
-
+    investors_to_terms.map { |name, terms| @safes.push(SAFE.new(name, *terms)) }
   end
 
   # Part 3: Add a priced round
@@ -123,9 +158,58 @@ class SimpleCap
   # Return the post-money valuation as a float
   # e.g. 98_765_432.1012
   def add_priced_round(investors_to_paid_amounts, pre_money_valuation)
+    # 1. Determine the price-per-share
+    total_shares = @shareholders.sum(&:num_shares)
+    price_per_share = pre_money_valuation / total_shares
 
-    # your code here...
+    # 2. Convert the SAFEs into shareholders
+    @safes.map do |safe|
+      # Question: What should we do if a SAFE has *both* a discount and cap?
+      # Answer:   Use either the discount OR cap, whichever yields a lower price for the investor
 
+      discounted_price_per_share = price_per_share * (1 - safe.discount)
+      capped_price_per_share = if safe.cap > 0 && safe.cap < pre_money_valuation
+        safe.cap / total_shares
+      else
+        price_per_share
+      end
+
+      # Question: What if the original price-per-share is lower than the capped price?
+      # Answer:   Use whichever yields the lower price for the investor
+      actual_price_per_share = [
+        price_per_share,
+        discounted_price_per_share,
+        capped_price_per_share,
+      ].min
+
+      @shareholders.push(Shareholder.new(
+        safe.name,
+        (safe.paid_amount / actual_price_per_share).round,
+        0.0, # to be recalculated
+        price_per_share,
+      ))
+    end
+
+    # 3. Convert the new investors into shareholders
+    investors_to_paid_amounts.map do |name, paid_amount|
+      @shareholders.push(Shareholder.new(
+        name,
+        (paid_amount / price_per_share).round,
+        0.0, # to be recalculated
+        price_per_share,
+      ))
+    end
+
+    # 4. Re-calculate the percentage ownership, price, and value for each shareholder
+    total_post_money_shares = @shareholders.sum(&:num_shares)
+    @shareholders.map do |shareholder|
+      shareholder.percent = (shareholder.num_shares / total_post_money_shares).round(4)
+      shareholder.price = price_per_share
+      shareholder.recalculate_value
+    end
+
+    # Return the post-money valuation as a float
+    @shareholders.sum(&:value)
   end
 
   # Bonus discussion
@@ -138,17 +222,20 @@ sc = SimpleCap.new({
   'Jill' => 0.48,
   'Jack' => 0.32,
 }, 10_000_000, 0.001)
+# puts sc.shareholders
 
 # Part 2
-# sc.add_safes({
-#   'Opaque Ventures'  => [500_000.0,   0.2,  0.0],          # 20% discount, no cap
-#   'BlackBox Capital' => [1_000_000.0, 0.0,  10_000_000.0], # no discount, 10M cap
-#   'Sandy Hills VC'   => [1_500_000.0, 0.15, 20_000_000.0], # 15% discount, 20M cap
-# })
+sc.add_safes({
+  'Opaque Ventures'  => [500_000.0,   0.2,  0.0],          # 20% discount, no cap
+  'BlackBox Capital' => [1_000_000.0, 0.0,  10_000_000.0], # no discount, 10M cap
+  'Sandy Hills VC'   => [1_500_000.0, 0.15, 20_000_000.0], # 15% discount, 20M cap
+})
+# puts sc.safes
 
 # Part 3
-# puts sc.add_priced_round({
-#   'Cormorant Ventures' => 4_000_000.0,
-#   'Provident Capital'  => 2_000_000.0,
-#   'BlackBox Capital'   => 1_000_000.0,
-# }, 15000000.0)
+puts sc.add_priced_round({
+  'Cormorant Ventures' => 4_000_000.0,
+  'Provident Capital'  => 2_000_000.0,
+  'BlackBox Capital'   => 1_000_000.0,
+}, 15000000.0)
+# puts sc.shareholders
